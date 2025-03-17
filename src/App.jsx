a1d40524b82e0e9f1c06b3985c3616eca528b521
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2025-03-17 15:57:28 +0100

    simple search icon

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import HiddenSearchBar from './components/HiddenSearchBar';
+
+function App() {
+  return (
+    <div>
+      <HiddenSearchBar />
+    </div>
+  );
+}
+
+export default App;
